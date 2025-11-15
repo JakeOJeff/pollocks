afd80b8947a1bfac77663a6f615817bdbf92a44f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 13:35:37 +0530

    Setting rotation and velocity

--- controller.lua
+++ controller.lua
@@ -5,14 +5,17 @@ function controller:load()
     self.x = 0
     self.y = 0
     self.img = love.graphics.newImage("character.png")
-    self.width = 100
-    self.height = 100
+    self.width = 50
+    self.height = 50
+    self.rotation = 0
+    self.xVel = 0
+    self.yVel = 0
     self.scaleX = self.width/self.img:getWidth()
     self.scaleY = self.height/self.img:getHeight()
 end
 
 function controller:draw()
-    love.graphics.draw(self.img, self.x, self.y, 0, self.scaleX, self.scaleY)
+    love.graphics.draw(self.img, self.x, self.y, self.rotation, self.scaleX, self.scaleY)
 end
 
 return controller
