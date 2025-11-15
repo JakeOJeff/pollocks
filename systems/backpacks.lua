64385b62e77aaf9134e3ca70fef0a990fad81dcd
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 23:35:06 +0530

    Setting packs

--- systems/backpacks.lua
+++ systems/backpacks.lua
@@ -1,15 +1,27 @@
 local backpacks = {}
 
 function backpacks:load()
-    self.
+    self.inventory = 0
     self.packs = {
         {
-            name = "Basic",
+            name = "Cloth",
             cost = 0,
             capacity = 10,
             id = 1,
             equipped = false,
             owned = true
+        },
+        {
+            name = "Canvas",
+            cost = 200,
+            capacity = 100,
+            id = 2,
+            equipped = false,
+            owned = false
+        },
+        {
+            name = "Reinforced Canvas",
+            
         }
     }
 end
