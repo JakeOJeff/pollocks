ca7b8894fbfec077ff020826cae1fdc526568aa8
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 13:33:23 +0530

    Setting and drawing controller

--- controller.lua
+++ controller.lua
@@ -4,5 +4,15 @@ local controller = {}
 function controller:load()
     self.x = 0
     self.y = 0
-    self.img = 
+    self.img = love.graphics.newImage("character.png")
+    self.width = 100
+    self.height = 100
+    self.scaleX = self.width/self.img:getWidth()
+    self.scaleY = self.height/self.img:getHeight()
 end
+
+function controller:draw()
+    love.graphics.draw(self.img, self.x, self.y, 0, self.scaleX, self.scaleY)
+end
+
+return controller
---
+++ main.lua
@@ -0,0 +1,10 @@
+local controller = require "controller"
+
+function love.load()
+    controller:load()
+end
+
+
+function love.draw()
+    controller:draw()
+end
