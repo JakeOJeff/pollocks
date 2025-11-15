d6130b0c251f1c734ff70774f2d5ff431d7f92d8
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 20:19:35 +0530

    Setting backpack

---
+++ systems/backpacks.lua
@@ -0,0 +1,18 @@
+local backpacks = {}
+
+function backpacks:load()
+    self.
+    self.packs = {
+        {
+            name = "Basic",
+            cost = 0,
+            capacity = 10,
+            id = 1,
+            equipped = false,
+            owned = true
+        }
+    }
+end
+
+
+return backpacks
