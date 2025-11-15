6c56837ab6c0fe9e7164d307101aaef2f4816dbc
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 23:42:41 +0530

    Adding 2 Items

--- systems/backpacks.lua
+++ systems/backpacks.lua
@@ -2,13 +2,14 @@ local backpacks = {}
 
 function backpacks:load()
     self.inventory = 0
+    self.equipped = 1
+    self.owned = {}
     self.packs = {
         {
             name = "Cloth",
             cost = 0,
             capacity = 10,
             id = 1,
-            equipped = false,
             owned = true
         },
         {
@@ -16,11 +17,30 @@ function backpacks:load()
             cost = 200,
             capacity = 100,
             id = 2,
-            equipped = false,
             owned = false
         },
         {
             name = "Reinforced Canvas",
+            cost = 1250,
+            capacity = 650,
+            id = 3,
+            owned = false,
+        },
+        {
+            name = "Ripstop Nylon",
+            cost = 3500,
+            capacity = 2000,
+            id = 4,
+            owned = false
+        },
+        {
+            name = "Cordura",
+            cost = 8000,
+            capacity = 10000,
+            id = 5,
+            owned = false
+        },
+        {
             
         }
     }
