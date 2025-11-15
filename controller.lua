d534ea2aef17fc46de95bd60472b6bfcf8cd7fb9
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 15:21:13 +0530

    Setting working Controller

--- controller.lua
+++ controller.lua
@@ -12,25 +12,37 @@ function controller:load()
     self.yVel = 0
     self.scaleX = self.width/self.img:getWidth()
     self.scaleY = self.height/self.img:getHeight()
+
+    self.acceleration = 200
+    self.friction = 300
 end
 
 function controller:update(dt)
-    self.y = self.y + self.yVel
-    self.yVel = self.yVel - 5 * dt
-    if love.keyboard.isDown("s") then
-        self.yVel = self.yVel - 20 * dt
-    elseif love.graphics.isDown("w") then
-        self.yVel = self.yVel + 20 * dt
+    -- Movement
+    if love.keyboard.isDown("w") then
+        self.yVel = self.yVel - self.acceleration * dt
+    elseif love.keyboard.isDown("s") then
+        self.yVel = self.yVel + self.acceleration * dt
+    else
+
+        if self.yVel > 0 then
+            self.yVel = math.max(0, self.yVel - self.friction * dt)
+        elseif self.yVel < 0 then
+            self.yVel = math.min(0, self.yVel + self.friction * dt)
+        end
     end
-    if love.graphics.isDown("a") then
+
+    if love.keyboard.isDown("a") then
         self.rotation = self.rotation - 10 * dt
-    elseif love.graphics.isDown("d") then
+    elseif love.keyboard.isDown("d") then
         self.rotation = self.rotation + 10 * dt
     end
+
+    self.y = self.y + self.yVel * dt
 end
 
 function controller:draw()
-    love.graphics.draw(self.img, self.x, self.y, self.rotation, self.scaleX, self.scaleY)
+    love.graphics.draw(self.img, self.x, self.y, math.rad(self.rotation), self.scaleX, self.scaleY)
 end
 
 return controller
--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ local controller = require "controller"
 function love.load()
     controller:load()
 end
-
+function love.update(dt)
+    controller:update(dt)
+end
 
 function love.draw()
     controller:draw()
