c06549d9cee23f51bec1710bd553504a5bbf1668
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-15 18:51:12 +0530

    Updating controllerp articles

--- controller.lua
+++ controller.lua
@@ -2,8 +2,8 @@ local controller = {}
 
 
 function controller:load()
-    self.x = 0
-    self.y = 0
+    self.x = wW / 2
+    self.y = wH / 2
     self.img = love.graphics.newImage("character.png")
     self.width = 50
     self.height = 50
@@ -14,21 +14,32 @@ function controller:load()
     self.scaleY = self.height/self.img:getHeight()
 
     self.acceleration = 200
-    self.friction = 300
+    self.friction = 500
+
+    self.particles = {}
+    
 end
 
 function controller:update(dt)
+    for i = 1, 60 do
+        table.insert(self.particles, {
+            x = self.x,
+            y = self.y,
+            life = 0,
+            maxLife = 1
+        })
+    end
     -- Turning
 
-    if love.keyboard.isDown("a") then
-        self.rotation = self.rotation - 90 * dt
-    elseif love.keyboard.isDown("d") then
-        self.rotation = self.rotation + 90 * dt
+    if love.keyboard.isDown("d") then
+        self.rotation = self.rotation - math.rad(90) * dt
+    elseif love.keyboard.isDown("a") then
+        self.rotation = self.rotation + math.rad(90) * dt
     end
 
 
-    local dx = math.cos(self.rotation)
-    local dy = math.sin(self.rotation)
+    local dx = math.sin(self.rotation)
+    local dy = math.cos(self.rotation)
 
     -- Movement
     if love.keyboard.isDown("w") then
@@ -41,18 +52,20 @@ function controller:update(dt)
 
         if self.yVel > 0 then
             self.yVel = math.max(0, self.yVel - self.friction * dt)
+            self.xVel = math.max(0, self.xVel - self.friction * dt)
         elseif self.yVel < 0 then
             self.yVel = math.min(0, self.yVel + self.friction * dt)
+            self.xVel = math.min(0, self.xVel + self.friction * dt)
         end
     end
 
 
-    self.x = self.x + self.xVel * dt
-    self.y = self.y + self.yVel * dt
+    self.x = (self.x + self.xVel * dt) % wW
+    self.y = (self.y + self.yVel * dt) % wH
 end
 
 function controller:draw()
-    love.graphics.draw(self.img, self.x, self.y, math.rad(self.rotation), self.scaleX, self.scaleY)
+    love.graphics.draw(self.img, self.x, self.y, -self.rotation, self.scaleX, self.scaleY)
 end
 
 return controller
