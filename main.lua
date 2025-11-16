5734077b156ae42fded9c66a6d96531f7019c270
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-11-16 11:24:40 +0530

    Drawing the star system

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local controller = require "controller"
 local backpack = require "systems.backpack"
+local stars = require "systems.stars"
 
 wW = love.graphics.getWidth()
 wH = love.graphics.getHeight()
@@ -7,12 +8,14 @@ wH = love.graphics.getHeight()
 function love.load()
     controller:load()
     backpack:load()
+    stars:load()
 end
 function love.update(dt)
     controller:update(dt)
 end
 
 function love.draw()
+    stars:draw()
     love.graphics.print(backpack.inventory.."/"..backpack.packs[backpack.equipped].capacity)
     controller:draw()
 end
--- systems/stars.lua
+++ systems/stars.lua
@@ -5,7 +5,18 @@ function stars:load()
     self.sets = {}
     for i = 1, 200 do
         table.insert(self.sets, {
-            
+            x = love.math.random(0, wW),
+            y = love.math.random(0, wH),
+            size = love.math.random(0, 3)
         })
     end
 end
+
+
+function stars:draw()
+    for i, v in ipairs(self.sets) do
+        love.graphics.circle("fill",v.x, v.y, v.size)
+    end
+end
+
+return stars
