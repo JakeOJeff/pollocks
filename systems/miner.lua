4eff059f80c05a4b934dd01d7684bfb4cb730091
Paulyn <realjakeojeff@gmail.com>
2025-11-17 07:04:48 +0530

    Added new Items

--- systems/miner.lua
+++ systems/miner.lua
@@ -6,7 +6,52 @@ function miner:load()
     self.owned = {}
     self.tools = {
         {
-            name = ""
+            name = "Scrap Hands",
+            cost = 0,
+            power = 1,
+            efficiency = 1,
+            id = 1,
+            owned = true
+        },
+        {
+            name = "Rusty Drill",
+            cost = 150,
+            power = 5,
+            efficiency = 2,
+            id = 2,
+            owned = false
+        },
+        {
+            name = "Standard Drill",
+            cost = 1200,
+            power = 20,
+            efficiency = 5,
+            id = 3,
+            owned = false
+        },
+        {
+            name = "Advanced Drill",
+            cost = 13500,
+            power = 90,
+            efficiency = 15,
+            id = 4,
+            owned = false
+        },
+        {
+            name = "Giant Saw",
+            cost = 60000,
+            power = 230,
+            efficiency = 50,
+            id = 5,
+            owned = false
+        },
+        {
+            name = "Plasma Cutter",
+            cost = 250000,
+            power = 800,
+            efficiency = 120,
+            id = 6,
+            owned = false
         }
     }
 end
